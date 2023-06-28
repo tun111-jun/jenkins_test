0b239c4048935a44b12bdb3ef021db6f80260b7e
yunjjung <yjung203@naver.com>
2023-06-28 17:29:24 +0900

    initial setting

---
+++ src/main/java/com/uplog/uplog/UplogApplication.java
@@ -0,0 +1,13 @@
+package com.uplog.uplog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UplogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UplogApplication.class, args);
+	}
+
+}
